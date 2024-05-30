4cf1badcf9a5b26cadd6de563b5548b577298aef
Henry Wu <wufuheng@gmail.com>
2024-05-30 13:54:40 -0700

    codeqwenchat

--- source/ch5-transformer/attention/draw_attn_map_qwenchat.py
+++ source/ch5-transformer/attention/draw_attn_map_qwenchat.py
@@ -1,6 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from util import plot_attention_map
+from util import plot_attention_map, plot_attention_rank
 from hiq.vis import print_model
+from hiq import read_file
 
 device = "cuda" # the device to load the model onto
 
@@ -17,8 +18,10 @@ You are a helpful assistant.<|im_end|>
 <|im_start|>user
 write a quick sort algorithm.<|im_end|>
 <|im_start|>assistant"""
-print(f"len(sentence): {len(sentence)}")
+
 inputs = tokenizer(sentence, return_tensors='pt')
+
+print(f"len(sentence): {len(sentence)}")
 print(f"inputs.shape:{inputs['input_ids'].shape}")
 
 outputs = model(input_ids=inputs['input_ids'],
@@ -29,15 +32,24 @@ outputs = model(input_ids=inputs['input_ids'],
                 use_cache=True)
 attention = outputs.attentions
 
-
-
 plot_attention_map(tokenizer, attention, inputs, "CodeQWen1.5-7B-Chat", figsize=20)
-
 
 # Decode the generated text
 print(f"outputs.logits.shape: {outputs.logits.shape}")
 generated_text = tokenizer.decode(outputs.logits.argmax(dim=-1).squeeze(), skip_special_tokens=True)
-
 print("*"*80)
 print(generated_text)
 
+#####################################################################################
+sentence = read_file("500.txt", by_line=False)
+inputs = tokenizer(sentence, return_tensors='pt')
+
+# Get the attention weights
+outputs = model(input_ids=inputs['input_ids'],
+                attention_mask = inputs['attention_mask'],
+                return_dict=True,
+                output_attentions=True,
+                output_hidden_states=False,
+                use_cache=True)
+attention = outputs.attentions
+plot_attention_rank(attention, "CodeQWen1.5-7B-Chat", "CodeQWen1.5-7B-Chat_attn_map_rank.png")
