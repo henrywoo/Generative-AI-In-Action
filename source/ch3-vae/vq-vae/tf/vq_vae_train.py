d0b92679ed511007e464700680a601e385f6fd16
Henry Wu <wufuheng@gmail.com>
2024-06-04 20:55:20 -0700

    vq-vae

--- source/ch3-vae/vq-vae/tf/vq_vae_train.py
+++ source/ch3-vae/vq-vae/tf/vq_vae_train.py
@@ -35,24 +35,17 @@ class VQVAETrainer(keras.models.Model):
 
     def train_step(self, x):
         with tf.GradientTape() as tape:
-            # Outputs from the VQ-VAE.
             reconstructions = self.vqvae(x)
-
             # Calculate the losses.
-            reconstruction_loss = (
-                    tf.reduce_mean((x - reconstructions) ** 2) / self.train_variance
-            )
+            reconstruction_loss = tf.reduce_mean((x - reconstructions) ** 2) / self.train_variance
             total_loss = reconstruction_loss + sum(self.vqvae.losses)
-
         # Backpropagation.
         grads = tape.gradient(total_loss, self.vqvae.trainable_variables)
         self.optimizer.apply_gradients(zip(grads, self.vqvae.trainable_variables))
-
         # Loss tracking.
         self.total_loss_tracker.update_state(total_loss)
         self.reconstruction_loss_tracker.update_state(reconstruction_loss)
         self.vq_loss_tracker.update_state(sum(self.vqvae.losses))
-
         # Log results.
         return {
             "loss": self.total_loss_tracker.result(),
@@ -149,14 +142,13 @@ def train_vqvae(epochs=10, f='model_vqvae.h5'):
     x_train_scaled = (x_train / 255.0) - 0.5
     x_test_scaled = (x_test / 255.0) - 0.5
     data_variance = np.var(x_train / 255.0)
-    get_vqvae().summary()
+    #get_vqvae().summary()
 
     vqvae_trainer = VQVAETrainer(data_variance, latent_dim=16, num_embeddings=num_embeddings)
     vqvae_trainer.compile(optimizer=keras.optimizers.Adam())
 
     if not os.path.exists(f):
         vqvae_trainer.fit(x_train_scaled, epochs=epochs, batch_size=128)
-
         trained_vqvae_model = vqvae_trainer.vqvae
         idx = np.random.choice(len(x_test_scaled), 10)
         test_images = x_test_scaled[idx]
@@ -176,28 +168,21 @@ def train_vqvae(epochs=10, f='model_vqvae.h5'):
     encoder = trained_vqvae_model.get_layer("encoder")
     quantizer = trained_vqvae_model.get_layer("vector_quantizer")
 
-    encoded_outputs = encoder.predict(test_images)
-    flat_enc_outputs = encoded_outputs.reshape(-1, encoded_outputs.shape[-1])
-    codebook_indices = quantizer.get_code_indices(flat_enc_outputs)
-    codebook_indices = codebook_indices.numpy().reshape(encoded_outputs.shape[:-1])
-
-    num_residual_blocks = 2
-    num_pixelcnn_layers = 2
-    pixelcnn_input_shape = encoded_outputs.shape[1:-1]
-    print(f"Input shape of the PixelCNN: {pixelcnn_input_shape}")
-
     # Generate the codebook indices.
     encoded_outputs = encoder.predict(x_train_scaled)
     flat_enc_outputs = encoded_outputs.reshape(-1, encoded_outputs.shape[-1])
     codebook_indices = quantizer.get_code_indices(flat_enc_outputs)
     codebook_indices = codebook_indices.numpy().reshape(encoded_outputs.shape[:-1])
-
-    return vqvae_trainer, quantizer, codebook_indices, pixelcnn_input_shape, num_residual_blocks, num_pixelcnn_layers, encoded_outputs
+    pixelcnn_input_shape = encoded_outputs.shape[1:-1]
+    print(f"Input shape of the PixelCNN: {pixelcnn_input_shape}")
+    return vqvae_trainer, quantizer, codebook_indices, pixelcnn_input_shape, encoded_outputs
 
 
 if __name__ == "__main__":
-    vqvae_trainer, quantizer, codebook_indices, pixelcnn_input_shape, num_residual_blocks, num_pixelcnn_layers, encoded_outputs = train_vqvae(epochs=100)
+    vqvae_trainer, quantizer, codebook_indices, pixelcnn_input_shape, encoded_outputs = train_vqvae(epochs=100)
     print(f"Shape of the training data for PixelCNN: {codebook_indices.shape}")
+    num_residual_blocks = 2
+    num_pixelcnn_layers = 2
     pixel_cnn = get_pixelcnn(pixelcnn_input_shape, num_residual_blocks, num_pixelcnn_layers, num_embeddings)
     pixel_cnn.compile(
         optimizer=keras.optimizers.Adam(3e-4),
