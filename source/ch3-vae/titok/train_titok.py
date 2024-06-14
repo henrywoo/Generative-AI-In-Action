a71417e1e33c8c9c774d1655721cd4d267825bae
Shadow Walker <shadowwalker2718@gmail.com>
2024-06-14 07:00:15 -0700

    titok

--- source/ch3-vae/titok/train_titok.py
+++ source/ch3-vae/titok/train_titok.py
@@ -3,22 +3,15 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
-from vit_pytorch import ViT
-from tqdm import tqdm
 from models import TiTok
-from proxy import get_proxy_codes, load_vqgan_model
+from proxy import load_vqgan_model
 from hiq.vis import print_model
-
-import torch.nn.functional as F
-
-import torch.optim as optim
 from tqdm import tqdm
 
 
-def warmup_training(model, dataloader, optimizer, num_epochs):
+def warmup_training(model, dataloader, optimizer, scheduler, num_epochs):
     model.train()
     mse_loss = nn.MSELoss()
-
     for epoch in range(num_epochs):
         total_loss = 0
         for images, _ in tqdm(dataloader):
@@ -29,14 +22,15 @@ def warmup_training(model, dataloader, optimizer, num_epochs):
             loss.backward()
             optimizer.step()
             total_loss += loss.item()
+        scheduler.step()
         avg_loss = total_loss / len(dataloader)
         print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {avg_loss:.4f}')
 
 
 if __name__ == "__main__":
-    # Define the training parameters
     batch_size = 64
     learning_rate = 1e-4
+    weight_decay = 1e-4
     num_epochs_warmup = 10
     num_epochs_finetune = 10
     latent_dim = 256
@@ -59,7 +53,6 @@ if __name__ == "__main__":
     test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # Device
     vqgan_model = load_vqgan_model('VQGAN/model.yaml', 'VQGAN/last.ckpt').to(device)
 
     model = TiTok(
@@ -69,10 +62,13 @@ if __name__ == "__main__":
         depth=6,
         heads=16,
         mlp_dim=2048,
-        K=8,
+        K=32,
         codebook=vqgan_model.quantize.embedding.weight,
     ).to(device)
 
     print_model(model)
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    warmup_training(model, train_loader, optimizer, num_epochs_warmup)
+
+    optimizer = optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_epochs_warmup)
+
+    warmup_training(model, train_loader, optimizer, scheduler, num_epochs_warmup)
