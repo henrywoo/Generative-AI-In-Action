d179dfde61ed79eab43b42adab7ff46a04b75e5b
Fuheng Wu <fuheng.wu@oracle.com>
2024-06-19 23:37:18 -0700

    perceptual loss

--- source/ch3-vae/titok/loss.py
+++ source/ch3-vae/titok/loss.py
@@ -2,27 +2,24 @@ import torch
 import torch.nn.functional as F
 from torchvision import models
 
-vgg = None
+vgg, mean_, std_ = None, None, None
 
 
 def perceptual_loss(reconstructed, original):
-    global vgg
+    global vgg, mean_, std_
     if vgg is None:
         device = reconstructed.device
-        # Load a pre-trained VGG model
         vgg = models.vgg16(weights='IMAGENET1K_V1').features
         vgg.to(device)
         vgg.eval()
         # Normalize the images in the same way as VGG was trained
-        mean = torch.tensor([0.485, 0.456, 0.406], device=device).view(1, 3, 1, 1)
-        std = torch.tensor([0.229, 0.224, 0.225], device=device).view(1, 3, 1, 1)
-    reconstructed = (reconstructed - mean) / std
-    original = (original - mean) / std
-
+        mean_ = torch.tensor([0.485, 0.456, 0.406], device=device).view(1, 3, 1, 1)
+        std_ = torch.tensor([0.229, 0.224, 0.225], device=device).view(1, 3, 1, 1)
+    reconstructed = (reconstructed - mean_) / std_
+    original = (original - mean_) / std_
     # Extract features from VGG
     features_reconstructed = vgg(reconstructed)
     features_original = vgg(original)
-
     # Compute the perceptual loss
     loss = F.mse_loss(features_reconstructed, features_original)
     return loss
